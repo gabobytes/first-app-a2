af05b7e22f3a9f357367420e3bb484fcf7b798f8
Gabriel Melo <gabrielmeloarteaga@gmail.com>
2017-01-29 09:24:54 -0500

    using templates and styles inline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+          <h1>Inline template!</h1>
+  `,
+  styles: [`
+       h1{ color:red};
+  `]
 })
 export class AppComponent {
   title = 'I changed this, again!';
